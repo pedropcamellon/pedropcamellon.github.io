7dfa854776819616a06ce224ed8033a710c7a6b7
Pedro P. Camellon <pedropcamellon@gmail.com>
2023-12-31 14:18:14 -0500

    Setup personal info and first sample post.

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,17 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Pedro P. Camellon - Blog',
+    description: 'Personal blog for sharing my thoughts and projects.',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
-      width: 400,
-      height: 300
+      width: 500,
+      height: 500
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
+        enabled: false, // possible value are : true | false
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: 'alpine' // alt of the logo
@@ -18,7 +19,7 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
@@ -26,16 +27,20 @@ export default defineAppConfig({
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'pedropcamellon',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://www.linkedin.com/in/pedro-pablo-camellon-328744234'
       }
     },
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+    // Disable back to top button: false
+    backToTop: {
+      text: 'Back to top',
+      icon: 'material-symbols:arrow-upward'
     }
   }
 })
